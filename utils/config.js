badb8c91f44d720ba41b30f0dc02b946f3b14f2c
XanxusX <tako30215@hotmail.com>
2020-12-21 11:25:56 +0800

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 export const host = "jiy.pdis.dev";
 export const jvb = host;
 export const muc = "conference." + host;
-export const welcome = '歡迎來到第xx次開放政府協作會議'
-export const title = '向海致敬'
+export const welcome = '歡迎來到第83次開放政府協作會議'
+export const title = '免疫新藥給付放寬案'
 export const description1 = '目前議程已到下半場分小組討論時間'
 export const description2 = '請點選進入以下分組'
