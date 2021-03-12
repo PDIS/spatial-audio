0b59033df2ed09e894ff61930ad18146520778e4
XanxusX <tako30215@hotmail.com>
2021-03-12 12:03:25 +0800

    Update title

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
-export const host = "jiy.pdis.dev";
-export const jvb = host;
-export const muc = "conference." + host;
-export const welcome = '歡迎來到第82次開放政府協作會議'
-export const title = 'B肝疫苗40週年紀念活動'
+export const host = 'jiy.pdis.dev'
+export const jvb = host
+export const muc = 'conference.' + host
+export const welcome = '歡迎來到86次開放政府協作會議'
+export const title = '台灣茶風味系統精進案'
 export const description1 = '目前議程已到下半場分小組討論時間'
 export const description2 = '請點選進入以下分組'
